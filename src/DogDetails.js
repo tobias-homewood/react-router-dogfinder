6c044760dd2af217bf87fe43936904e37a867f3d
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-04 18:18:17 +0000

    dog details component

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import './DogDetails.scss'
 
-const DogDetails = () => {
+const DogDetails = ({dogs}) => {
     const { name } = useParams();
+    const dog = dogs.find(dog => dog.name === name);
   return (
-    <div>{name}</div>
+    <div className='dogdetails'>
+      <h1>{dog.name}</h1>
+      <img src={`/images/${dog.src}`} alt={dog.name} />
+      <h2>{dog.age} years old</h2>
+      <ul>
+        {dog.facts.map((fact, i) => (
+          <li key={i}>{fact}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
