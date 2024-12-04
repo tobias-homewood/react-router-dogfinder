5d433c953c83db6a7c816a1abeb0e7aab73f4484
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-04 18:29:54 +0000

    assert that a dog is found before rendering

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import './DogDetails.scss'
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import "./DogDetails.scss";
 
-const DogDetails = ({dogs}) => {
+const DogDetails = ({ dogs }) => {
     const { name } = useParams();
-    const dog = dogs.find(dog => dog.name === name);
-  return (
-    <div className='dogdetails'>
-      <h1>{dog.name}</h1>
-      <img src={`/images/${dog.src}`} alt={dog.name} />
-      <h2>{dog.age} years old</h2>
-      <ul>
-        {dog.facts.map((fact, i) => (
-          <li key={i}>{fact}</li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+    const dog = dogs.find((dog) => dog.name === name);
+    if (!dog) return <Navigate to="/dogs" />;
+    return (
+        <div className="dogdetails">
+            <h1>{dog.name}</h1>
+            <img src={`/images/${dog.src}`} alt={dog.name} />
+            <h2>{dog.age} years old</h2>
+            <ul>
+                {dog.facts.map((fact, i) => (
+                    <li key={i}>{fact}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
-export default DogDetails
+export default DogDetails;
