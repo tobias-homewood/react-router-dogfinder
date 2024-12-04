770ef34bbeb297a0a8089f9d66f28f023704b742
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-04 18:18:26 +0000

    dog list component

--- src/DogList.js
+++ src/DogList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import './DogList.scss';
 
-const DogList = () => {
+const DogList = ({dogs}) => {
     return (
-        <div>
-            <h1>Choose a dog!</h1>
+        <div className="doglist">
+            {dogs.map(dog => (
+                <div key={dog.name}>
+                    <h1>{dog.name}</h1>
+                    <img src={`/images/${dog.src}`} alt={dog.name} />
+                </div>
+            ))}
         </div>
     );
 };
